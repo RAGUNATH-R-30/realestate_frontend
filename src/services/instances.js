1171413117d4547ec5be5cfc33f0620dd01debc8
santhosh annamalai <santhoshmathi2002@gmail.com>
2024-07-25 16:57:51 +0530

    final before commit one

--- src/services/instances.js
+++ src/services/instances.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const baseURL = 'http://realestate-api-unse.onrender.com/api'
+const baseURL = 'https://realestate-api-unse.onrender.com/api'
 
 export const instance = axios.create({
     baseURL:baseURL,
